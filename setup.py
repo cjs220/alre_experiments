3c45d3906696c2d758c3adcb45d56753ef24551a
Conor Sheehan <ConorJamesSheehan@gmail.com>
2020-06-19 17:38:24 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     license='MIT',
     author='Conor Sheehan',
     author_email='conor.sheehan-2@manchester.ac.uk',
-    description=''
+    description='',
+    install_requires=['matplotlib', 'pandas', 'numpy', 'joblib', 'PyYAML', 'scipy', 'tensorflow',
+                      'active_learning_ratio_estimation']
 )
