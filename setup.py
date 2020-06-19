cfb8fa0c9bf2c9257686c5a5e7c102e87f2c074c
Conor Sheehan <ConorJamesSheehan@gmail.com>
2020-06-19 18:05:47 +0100

    added manifest.in

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     author_email='conor.sheehan-2@manchester.ac.uk',
     description='',
     install_requires=['matplotlib', 'pandas', 'numpy', 'joblib', 'PyYAML', 'scipy', 'tensorflow',
-                      'active_learning_ratio_estimation']
+                      'active_learning_ratio_estimation'],
+    include_package_data=True
 )
