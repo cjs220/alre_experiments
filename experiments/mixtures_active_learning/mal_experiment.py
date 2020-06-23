ca6a089d77bcb0c4d7277ec38e72632a13b6626f
Conor Sheehan <ConorJamesSheehan@gmail.com>
2020-06-23 15:06:35 +0100

    mal_experiment update + preprod config

--- experiments/mixtures_active_learning/mal_experiment.py
+++ experiments/mixtures_active_learning/mal_experiment.py
@@ -3,10 +3,15 @@ import time
 from logging import Logger
 from typing import Dict, List
 
+import numpy as np
+import pandas as pd
+from pandas.core.generic import NDFrame
+
 from active_learning_ratio_estimation.active_learning.active_learner import UpperConfidenceBoundLearner, \
     RandomActiveLearner
 from active_learning_ratio_estimation.dataset import ParamGrid
 from active_learning_ratio_estimation.model import FlipoutClassifier, SinglyParameterizedRatioModel
+from active_learning_ratio_estimation.model.ratio_model import exact_param_scan
 from util.distributions import triple_mixture
 
 
@@ -27,14 +32,21 @@ def run_mixtures_active_learning(
         n_iter: int,
         ucb_kappa: float,
         logger: Logger = None
-):
+) -> Dict[str, NDFrame]:
     logger = logger or logging.getLogger(__name__)
     logger.info('Starting experiment')
     t0 = time.time()
 
-    X_true = triple_mixture(theta_true).sample(n_true).numpy()
-
+    logger.info('Simulating X_true and performing exact param scan')
     param_grid = ParamGrid(bounds=[theta_bounds], num=n_theta)
+    X_true = triple_mixture(theta_true).sample(n_true).numpy()
+    nllr_exact, mle_exact = exact_param_scan(
+        simulator_func=triple_mixture,
+        X_true=X_true,
+        param_grid=param_grid,
+        theta_0=theta_0,
+        to_meshgrid_shape=False
+    )
 
     active_learners = dict(
         UCB=UpperConfidenceBoundLearner(
@@ -60,7 +72,42 @@ def run_mixtures_active_learning(
         ),
     )
 
+    logger.info('Fitting ActiveLearners.')
     for name, active_learner in active_learners.items():
+        logger.info(f'Fitting {name} ActiveLearner.')
         active_learner.fit(n_iter=n_iter)
 
-    pass
+    logger.info('Finished fitting, collecting results.')
+
+    mle = pd.DataFrame(
+        {learner_name: map(float, learner.mle_predictions)
+         for learner_name, learner in active_learners.items()}
+    )
+    mle['Exact'] = float(mle_exact)
+
+    trialed_thetas = pd.DataFrame(
+        {learner_name: map(float, learner.trialed_thetas)
+         for learner_name, learner in active_learners.items()}
+    )
+
+    def _collect_nllr_info(learner_name, info):
+        return pd.DataFrame(
+            data=np.stack(getattr(active_learners[learner_name], info), axis=1),
+            columns=[f'Iteration {i}' for i in range(n_iter + 1)],
+            index=param_grid.array.squeeze()
+        )
+
+    random_nllr = _collect_nllr_info('Random', 'nllr_predictions')
+    ucb_nllr = _collect_nllr_info('UCB', 'nllr_predictions')
+    ucb_std = _collect_nllr_info('UCB', 'nllr_std')
+    random_nllr['Exact'] = nllr_exact.squeeze()
+    ucb_nllr['Exact'] = nllr_exact.squeeze()
+
+    logger.info(f'Finished experiment; total time {int(time.time() - t0):.3E} s')
+    return dict(
+        mle=mle,
+        trialed_thetas=trialed_thetas,
+        random_nllr=random_nllr,
+        ucb_nllr=ucb_nllr,
+        ucb_std=ucb_std
+    )
